00f0820c1430bfd327cd0fa887fcad0c1c0f0e7f
Ashley Davis <ashley.davis@realseriousgames.com>
2016-04-01 14:21:28 +1000

    Added 2nd example: getting a result from a coroutine via a promise.

---
+++ Assets/2. CoroutineResult/CoroutineResult.cs
@@ -0,0 +1,40 @@
+﻿using RSG;
+using System;
+using System.Collections;
+using UnityEngine;
+
+//
+// Example of a coroutine wrapped in a promise that returns a result.
+//
+// This doesn't do anything much. It's just simply a template to start from.
+//
+public class CoroutineResult : MonoBehaviour
+{
+    public IPromise<string> Execute()
+    {
+        return new Promise<string>((resolve, reject) =>
+            StartCoroutine(TheCoroutine(resolve, reject))
+        );
+    }
+
+    private IEnumerator TheCoroutine(Action<string> resolve, Action<Exception> reject)
+    {
+        // ... add your async operations here ...
+
+        yield return new WaitForSeconds(5f);
+
+        // ... several yields later ...
+
+        var someErrorOccurred = false;
+        if (someErrorOccurred)
+        {
+            // An error occurred, reject the promise.
+            reject(new ApplicationException("My error"));
+        }
+        else
+        {
+            // Completed successfully, resolve the promise.
+            resolve("Hi from the coroutine!");
+        }
+    }
+}
---
+++ Assets/2. CoroutineResult/CoroutineResultExampleUsage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineResultExampleUsage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var exampleCoroutine = this.gameObject.AddComponent<CoroutineResult>();
+        Debug.Log("Starting coroutine...");
+        exampleCoroutine.Execute()
+            .Then(value => Debug.Log("Coroutine has completed with value: " + value)) // Signal completion!
+            .Catch(ex => Debug.LogException(ex, this)); // Handle any errors that may have occured.
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
