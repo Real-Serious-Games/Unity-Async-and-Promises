5bfaf56b19faad1c5f5bffeb6e1810c2f3a51cf4
Ashley Davis <ashley.davis@realseriousgames.com>
2016-04-01 14:13:45 +1000

    Added Unity project and first basic example of wrapping an Unity coroutine as a promise.

---
+++ Assets/1. BasicCoroutineAsPromise/BasicCoroutineAsPromise.cs
@@ -0,0 +1,40 @@
+﻿using RSG;
+using System;
+using System.Collections;
+using UnityEngine;
+
+//
+// Most basic example of wrapping a coroutine in a promise.
+//
+// This doesn't do anything. It's just simply a template to start from.
+//
+public class BasicCoroutineAsPromise : MonoBehaviour
+{
+    public IPromise Execute()
+    {
+        return new Promise((resolve, reject) =>
+            StartCoroutine(TheCoroutine(resolve, reject))
+        );
+    }
+
+    private IEnumerator TheCoroutine(Action resolve, Action<Exception> reject)
+    {
+        // ... add your async operations here ...
+
+        yield return new WaitForSeconds(5f);
+
+        // ... several yields later ...
+
+        var someErrorOccurred = false;
+        if (someErrorOccurred)
+        {
+            // An error occurred, reject the promise.
+            reject(new ApplicationException("My error"));
+        }
+        else
+        {
+            // Completed successfully, resolve the promise.
+            resolve();
+        }
+    }
+}
---
+++ Assets/1. BasicCoroutineAsPromise/BasicCoroutineAsPromiseExampleUsage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCoroutineAsPromiseExampleUsage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var exampleCoroutine = this.gameObject.AddComponent<BasicCoroutineAsPromise>();
+        Debug.Log("Starting coroutine...");
+        exampleCoroutine.Execute()
+            .Then(() => Debug.Log("Coroutine has completed!")) // Signal completion!
+            .Catch(ex => Debug.LogException(ex, this)); // Handle any errors that may have occured.
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
