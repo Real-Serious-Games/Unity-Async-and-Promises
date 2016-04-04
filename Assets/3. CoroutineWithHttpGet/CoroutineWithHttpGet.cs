2519ef29f0ce6c2e6ca5b337b00d9aad66142a27
Ashley Davis <ashley.davis@realseriousgames.com>
2016-04-04 11:35:39 +1000

    Added 2nd example: HTTP GET using Unity's WWW wrapped in a promise.

---
+++ Assets/3. CoroutineWithHttpGet/CoroutineWithHttpGet.cs
@@ -0,0 +1,44 @@
+﻿using RSG;
+using System;
+using System.Collections;
+using UnityEngine;
+
+//
+// Example of a HTTP GET using Unity's WWW wrapped in a promise.
+//
+public class CoroutineWithHttpGet : MonoBehaviour
+{
+    /// <summary>
+    /// Returns a promise with the deserialised result of a GET request to the specified URL.
+    /// The result of the raw request must be JSON that can be deserialised into type T.
+    /// </summary>
+    public IPromise<string> Get(string url)
+    {
+        return new Promise<string>((resolve, reject) =>
+            StartCoroutine(TheCoroutine(url, resolve, reject))
+        );
+    }
+
+    private IEnumerator TheCoroutine(string url, Action<string> resolve, Action<Exception> reject)
+    {
+        var www = new WWW(url);
+
+        yield return www; // Allow the async operation to complete.
+
+        try
+        {
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                reject(new ApplicationException(www.error));
+            }
+            else
+            {
+                resolve(www.text);
+            }
+        }
+        catch (Exception ex)
+        {
+            reject(ex);
+        }
+    }
+}
---
+++ Assets/3. CoroutineWithHttpGet/CoroutineWithHttpGetExampleUsage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineWithHttpGetExampleUsage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var exampleCoroutine = this.gameObject.AddComponent<CoroutineWithHttpGet>();
+        Debug.Log("Starting coroutine...");
+        exampleCoroutine.Get("http://www.google.com")
+            .Then(value => Debug.Log("HTML: " + value)) // Signal completion!
+            .Catch(ex => Debug.LogException(ex, this)); // Handle any errors that may have occured.
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
